4e0f307ea9fc09c8ab19506631e3a890d3877acd
borntohonk <6264306+borntohonk@users.noreply.github.com>
2021-10-03 14:23:53 +0200

    fix incorrect duplicate patches between 9.0.0 and 12.1.0, tweak regex

--- organize/organize.py
+++ organize/organize.py
@@ -283,7 +283,7 @@ for version in os.listdir("."):
 
     with open(esuncompressed, 'rb') as fi:
         read_data = fi.read()
-    result = re.search(b'\x1f\x01\x09\xeb\x61\x00\x00\x54\xf3\x03\x1f\xaa\x02\x00\x00\x14\x33\xd2\x85\x52\xe0.\x00\x91.{3}\x94.\x63\x00.{3}\x00\x94\xa0.{2}\xd1.{2}\xff\x97', read_data)
+    result = re.search(b'.\x63\x00.{3}\x00\x94\xa0.{2}\xd1.{2}\xff\x97', read_data)
     patch = "%06X%s%s" % (result.end(), "0004", "E0031FAA")
     text_file = open("../atmosphere/exefs_patches/es_patches/" + get_es_build_id() + ".ips", "wb")
     print("es build-id: " + get_es_build_id())
